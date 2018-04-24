acd99ee3fab8945d5c57c9d2093fa6bff958a563
Ondřej Duchoň <duchon.ondrej@gmail.com>
2018-04-24 23:56:47 +0200

    Tweaks

--- js/TreePresenter.js
+++ js/TreePresenter.js
@@ -1644,9 +1644,15 @@
 
                 headings.reverse();
 
+                let contents = slide.content.length;
+
+                if (slide.derivation === null) {
+                    contents--;
+                }
+
                 for (let i = 0; i < headings.length; i++) {
                     if (i === headings.length - 1) {
-                        this.droNav.innerHTML += '<a href="#' + headings[i].heading.id + '">' + headings[i].heading.innerText + '</a>';
+                        this.droNav.innerHTML += '<a href="#' + headings[i].heading.id + '">' + headings[i].heading.innerText + ' (' + contents + ')' + '</a>';
                     } else {
                         this.droNav.innerHTML += '<a href="#' + headings[i].heading.id + '">' + headings[i].heading.innerText + '</a>' + ' > ';
                     }
